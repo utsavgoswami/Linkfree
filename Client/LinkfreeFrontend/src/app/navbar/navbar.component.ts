9b9710abf876126a1c27836653a9f5df59dfe804
Utsav Goswami <utsavg19@googlemail.com>
2021-03-28 18:45:14 -0700

    Modified navbar look based on route

--- Client/LinkfreeFrontend/src/app/navbar/navbar.component.ts
+++ Client/LinkfreeFrontend/src/app/navbar/navbar.component.ts
@@ -5,10 +5,16 @@ import { Component, OnInit, Input } from '@angular/core';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
 
+  @Input() showNavbarItems: boolean = true;
   isLoggedIn: boolean;
   burgerMenuExpanded: boolean = true;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 
   toggleBurgerMenu() {
     this.burgerMenuExpanded = !this.burgerMenuExpanded;
